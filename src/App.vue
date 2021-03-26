67c147602d886450fe2caefc0f60d9aa621d27f4
airikavettik <airikavettik@Airikas-MBP.lan>
2021-03-26 13:47:08 +0200

    back-to-up button

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <v-app>
       <div class="container">
-      <img width="20%" src="./assets/calculator.png">
+      <img width="30%" src="./assets/calculator.png">
       <NavBar />
       
         
-
+    <back-to-top bottom="50px" right="50px">
+      <button type="button" class="btn btn-info btn-to-top"><i class="fa fa-chevron-up"></i>Up</button>
+    </back-to-top>
     </div>
   </v-app>
 </template>
 
 <script>
 import NavBar from "./components/NavBar"
+
 
 export default {
   name: "App",
@@ -27,4 +30,12 @@ export default {
     text-align: center;
     background: rgb(247, 247, 247);
   }
+  .btn-to-top {
+  width: 60px;
+  height: 60px;
+  padding: 10px 16px;
+  border-radius: 50%;
+  font-size: 22px;
+  line-height: 22px;
+}
 </style>
